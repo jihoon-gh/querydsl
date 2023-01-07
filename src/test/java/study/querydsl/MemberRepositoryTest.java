08cd83b2c8c12cce97eb40ee7cb7d5b9986fec40
jihoon-gh <jihoon806@gmail.com>
2023-01-07 23:28:50 +0900

    [FEAT] Querydsl with pure jpa complete

--- src/test/java/study/querydsl/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/MemberRepositoryTest.java
@@ -1,14 +1,21 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 import study.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -19,6 +26,8 @@ public class MemberRepositoryTest {
 
     @Autowired
     MemberJpaRepository memberJpaRepository;
+
+
 
     @Test
     public void basicTest(){
@@ -32,5 +41,30 @@ public class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(member1).isEqualTo(member);
+    }
+    @Test
+    public void searchTest(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(35);
+//        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
+
+        Assertions.assertThat(result).extracting("username").containsExactly("member3", "member4");
     }
 }
